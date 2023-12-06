ab9b2d8f3598d642431181863fd08b4cb967f88a
Aayon Mukherjee <aayonmukherjee21@gmail.com>
2023-12-06 14:06:38 -0500

    Update accordion.jsx

--- client/src/utils/accordion.jsx
+++ client/src/utils/accordion.jsx
@@ -5,19 +5,19 @@ const data = [
     icon: <HiShieldCheck/>,
     heading: "Best interest rates on the market",
     detail:
-      "Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat occaecat ut occaecat consequat est minim minim esse tempor laborum consequat esse adipisicing eu reprehenderit enim.",
+      "Unlock your dream home with our real estate expertise and secure the best interest rates for a brighter financial future.",
   },
   {
     icon: <MdCancel />,
     heading: "Prevent unstable prices",
     detail:
-      "Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat occaecat ut occaecat consequat est minim minim esse tempor laborum consequat esse adipisicing eu reprehenderit enim.",
+      "Stabilize your investment with our strategic approach, preventing the impact of unstable prices in the real estate market.",
   },
   {
     icon: <MdAnalytics />,
     heading: "Best price on the market",
     detail:
-      "Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat occaecat ut occaecat consequat est minim minim esse tempor laborum consequat esse adipisicing eu reprehenderit enim.",
+      "Discover the pinnacle of value with our real estate offerings – securing you the best price on the market for your dream property.",
   },
 ];
 export default data;
